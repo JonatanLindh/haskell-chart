d8eef9757a04592df2c5689349f8d170d4df9220
Jonatan Lindh <jonatan.lindh1@gmail.com>
2025-03-13 14:46:41 +0100

    nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.haskell-flake.flakeModule ];
+
+      perSystem = { self', pkgs, ... }: {
+
+        haskellProjects.default = {
+          devShell = {
+            mkShellArgs = rec {
+              buildInputs = with pkgs; [
+                pkg-config
+              ];
+
+              LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+            };
+          };
+        };
+
+        packages.default = self'.packages.Chart;
+      };
+    };
+}
